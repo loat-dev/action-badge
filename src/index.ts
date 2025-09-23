1087c9eadc9989215c733be84fed3ee47b8de253
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-09-23 15:55:41 +0200

    Update output name and generate SVG in action
    Renames the output from 'svg-output' to 'svg-text' in action.yml and updates src/index.ts to generate SVG using the new generateSVG function. The output value is now set to the generated SVG instead of a test string.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import actionsCore from '@actions/core';
+import { generateSVG } from './svg/generate_svg.ts';
 
-const statusTextInput = actionsCore.getInput('status-text');
-const statusColorInput = actionsCore.getInput('status-color');
 const lableTextInput = actionsCore.getInput('label-text');
 const iconPathInput = actionsCore.getInput('icon-path');
+const statusTextInput = actionsCore.getInput('status-text');
+const statusColorInput = actionsCore.getInput('status-color');
 
 console.log('status-text:', statusTextInput);
 console.log('status-color:', statusColorInput);
 console.log('label-text:', lableTextInput);
 console.log('icon-path:', iconPathInput);
 
-actionsCore.setOutput('custom-text-output', 'Test123');
+const svg = generateSVG({text: lableTextInput}, {text: statusTextInput});
+
+actionsCore.setOutput('svg-text', svg);
