856a79c169aeb6b1844c77993d996848081a36c9
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-09-23 16:28:41 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,4 @@ console.log('icon-path:', iconPathInput);
 
 const svg = generateSVG({text: lableTextInput}, {text: statusTextInput});
 
-actionsCore.setOutput('svg-text', svg.replace(/\n/gm, ''));
+actionsCore.setOutput('svg-text', encodeURIComponent(svg))
