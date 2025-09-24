89b9c0da26279a29062989cee34dafb856a84f21
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-09-24 10:28:37 +0200

    Update generate_svg.ts

--- src/svg/generate_svg.ts
+++ src/svg/generate_svg.ts
@@ -26,7 +26,8 @@ export function generateSVG(
              
   const workflowBackgroundPath = `M0,3 C0,1.3431 1.3552,0 3.02702703,0 L${workflowWidth},0 L${workflowWidth},20 L3.02702703,20 C1.3552,20 0,18.6569 0,17 L0,3 Z`;
   const stateBackgroundPath = `M0 0 H${stateWidth - 2.53} C${stateWidth - 1.132} 0 ${stateWidth} 1.343 ${stateWidth} 3 v14 c0 1.657-1.132 3-2.53 3 H0 V0 z`;
-
+  const statePath =           `M0 0h${stateWidth + 0.47}C${stateWidth + 1.869} 0 ${stateWidth + 3} 1.343 ${stateWidth + 3} 3v14c0 1.657-1.132 3-2.53 3H0V0z`
+                            // M0 0h40.47               C41.869                0 43                1.343 43                3v14c0 1.657-1.132 3-2.53 3H0V0z
   return `
     <svg xmlns="http://www.w3.org/2000/svg" width="${totalWidth}" height="20">
       <title>${label.text} - ${state.text}</title>
@@ -51,7 +52,7 @@ export function generateSVG(
           </text>
         </g>
         <g transform="translate(${workflowWidth})" font-family="'DejaVu Sans',Verdana,Geneva,sans-serif" font-size="11px">
-          <path id="state_background" d="${stateBackgroundPath}" fill="url(#state-fill)" fill-rule="nonzero"/>
+          <path id="state_background" d="${statePath}" fill="url(#state-fill)" fill-rule="nonzero"/>
           <text fill="#010101" fill-opacity=".3" aria-hidden="true">
             <tspan x="5" y="15">${state.text}</tspan>
           </text>
