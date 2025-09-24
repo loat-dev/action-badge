f97647498797fa1bc5d70ccdc21b00868ea081a1
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-09-24 11:33:28 +0200

    Rename status inputs to state in action and code
    Renamed 'status-text' and 'status-color' inputs to 'state-text' and 'state-color' in action.yml and updated references in src/index.ts for consistency. Also updated the SVG generation to use the new input names and pass backgroundColor.

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,15 @@ import { generateSVG } from './svg/generate_svg.ts';
 
 const lableTextInput = actionsCore.getInput('label-text');
 const iconPathInput = actionsCore.getInput('icon-path');
-const statusTextInput = actionsCore.getInput('status-text');
-const statusColorInput = actionsCore.getInput('status-color');
+const stateTextInput = actionsCore.getInput('status-text');
+const stateColorInput = actionsCore.getInput('status-color');
 
-console.log('status-text:', statusTextInput);
-console.log('status-color:', statusColorInput);
+console.log('state-text:', stateTextInput);
+console.log('state-color:', stateColorInput);
 console.log('label-text:', lableTextInput);
 console.log('icon-path:', iconPathInput);
 
-const svg = generateSVG({text: lableTextInput}, {text: statusTextInput});
+const svg = generateSVG({text: lableTextInput}, {text: stateTextInput, backgroundColor: stateColorInput});
 
 actionsCore.setOutput('svg-text', svg)
 actionsCore.setOutput('svg-text-encoded', encodeURIComponent(svg))
