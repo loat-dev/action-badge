3d78bfed04727fbcba1826e7ffb6ee7cc688eb38
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-09-25 14:48:13 +0200

    Add icon color option to SVG generation
    Introduces an 'icon-color' input to action.yml and updates SVG generation to support customizable icon colors. Refactors SVG generation to accept an IconOptions object, improving flexibility for icon rendering.

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ const stateTextColorInput = actionsCore.getInput('state-text-color');
 const stateBackgroundColorInput = actionsCore.getInput('state-background-color');
 
 const iconInput = actionsCore.getInput('icon');
+const iconColorInput = actionsCore.getInput('icon-color');
 
 console.log('label-text:', lableTextInput);
 console.log('label-text-color:', lableTextColorInput);
@@ -20,11 +21,12 @@ console.log('state-text:', stateTextInput);
 console.log('state-text-color:', stateTextColorInput);
 console.log('state-background-color:', stateBackgroundColorInput);
 console.log('icon:', iconInput);
+console.log('icon-color:', iconColorInput);
 
 const svg = generateSVG(
   {text: lableTextInput, backgroundColor: labelBackgroundColorInput},
   {text: stateTextInput, backgroundColor: stateBackgroundColorInput},
-  octIcons[iconInput as keyof typeof octIcons]?.heights?.['16']?.path
+  {path: octIcons[iconInput as keyof typeof octIcons]?.heights?.['16']?.path, color: iconColorInput}
 );
 
 actionsCore.setOutput('svg-text', svg)
---
+++ src/svg/icon_options.ts
@@ -0,0 +1,7 @@
+/**
+ * Options for the label
+ */
+export interface IconOptions {
+  path : string | undefined,
+  color : string
+}
